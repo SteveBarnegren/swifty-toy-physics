f621063542c3fb379fc9aec68e6937f1d5358f25
Steve Barnegren <steve.barnegren@gmail.com>
2020-02-22 16:45:39 +0000

    Created project

---
+++ PhysicsDemo/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  PhysicsDemo
+//
+//  Created by Steve Barnegren on 22/02/2020.
+//  Copyright © 2020 Steve Barnegren. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ PhysicsDemo/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  PhysicsDemo
+//
+//  Created by Steve Barnegren on 22/02/2020.
+//  Copyright © 2020 Steve Barnegren. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
